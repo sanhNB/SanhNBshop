157d1ee88dc96deda7e90c681636a01208eb8aef
unknown <sanhnbpk00900@fpt.edu.vn>
2019-11-07 15:38:06 +0700

    first commit

---
+++ dichvu123.js
@@ -0,0 +1,70 @@
+									[
+									{
+										"p_id": "1",
+										"tensp":"Iphone 8 Plus",
+										"hinhsp":"dt1.jpg",
+										"giasp": 12990000
+									},
+									
+									{
+										"p_id": "2",
+										"tensp":"Iphone 7Plus",
+										"hinhsp":"dt2.jpg",
+										"giasp": 7990000
+									},
+									
+									{
+										"p_id": "3",
+										"tensp:"Iphone X",
+										"hinhsp":"dt3.jpg",
+										"giasp": 16990000
+									},
+									
+									{
+										"p_id": "4",
+										"tensp":"Xiaomi MI8",
+										"hinhsp":"dt4.jpg",
+										"giasp": 10990000
+									},
+									
+									{
+										"p_id": "5",
+										"tensp":"Samsung galaxy note 9",
+										"hinhsp":"dt5.jpg",
+										"giasp": 14990000
+									},
+									
+									{
+										"p_id": "6",
+										"tensp":"Samsung galaxy S10",
+										"hinhsp":"dt6.jpg",
+										"giasp": 24990000
+									},
+									
+									{
+										"p_id": "7",
+										"tensp":"Iphone 6splus",
+										"hinhsp":"dt7.jpg",
+										"giasp": 5490000
+									},
+									
+									{
+										"p_id": "8",
+										"tensp":"Samsung galaxy A8 ",
+										"hinhsp":"dt8.jpg",
+										"giasp": 4390000
+									},
+									
+									{
+										"p_id": "9",
+										"tensp":"Cường lực Iphone X",
+										"hinhsp":"dt9.jpg",
+										"giasp": 169000
+									},
+									
+									{
+										"p_id": "10",
+										"tensp":"Xiaomi note 7",
+										"hinhsp":"dt10.jpg",
+										"giasp": 4290000
+									}]
---
+++ js/sphttp.js
@@ -0,0 +1,65 @@
+[
+								{
+									ma: "AD124",
+									ten: "Iphone X 128 GB",
+									gianhap: "14.500.000đ",
+									giaban: "16.990.000đ",
+									ngaynhap: "20/04/2019",
+									nhasanxuat: "Hoàng Hà Mobile"
+								},
+								{
+									ma: "SA121",
+									ten: "Iphone 8 Plus 64GB",
+									gianhap: "10.000.000đ",
+									giaban: "13.990.000đ",
+									ngaynhap: "20/04/2019",
+									nhasanxuat: "Hoàng Hà Mobile"
+								},
+								{
+									ma: "AS154",
+									ten: "Iphone 7Plus 128GB",
+									gianhap: "5.000.000đ",
+									giaban: "7.990.000đ",
+									ngaynhap: "22/04/2019",
+									nhasanxuat: "Hoàng Hà Mobile"
+								},
+								{
+									ma: "AA125",
+									ten: "Iphone 6S Plus",
+									gianhap: "2.500.000đ",
+									giaban: "4.540.000đ",
+									ngaynhap: "22/04/2019",
+									nhasanxuat: "Hoàng Hà Mobile"
+								},
+								{
+									ma: "AV147",
+									ten: "Samsung Galaxy S10",
+									gianhap: "20.500.000đ",
+									giaban: "24.990.000đ",
+									ngaynhap: "17/03/2019",
+									nhasanxuat: "Samsung Việt Nam"
+								},
+								{
+									ma: "AW111",
+									ten: "Samsung Galaxy A8",
+									gianhap: "6.000.000đ",
+									giaban: "7.490.000đ",
+									ngaynhap: "19/04/2019",
+									nhasanxuat: "Samsung việt Nam"
+								},
+								{
+									ma: "AG445",
+									ten: "Xiaomi Note 7",
+									gianhap: "3.100.000đ",
+									giaban: "4.590.000đ",
+									ngaynhap: "28/03/2019",
+									nhasanxuat: "Xiaomi Việt Nam"
+								},
+								{
+									ma: "AL442",
+									ten:"Xiaomi MI8",
+									gianhap: "9.200.000đ",
+									giaban: "11.490.000đ",
+									ngaynhap: "12/04/2019",
+									nhasanxuat: "Xiaomi Việt Nam"
+								}]
